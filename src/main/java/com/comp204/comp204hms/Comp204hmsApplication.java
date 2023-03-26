92002c5190d9a5a2a1ab0efe558d296c18b9680e
BBerkeYildirim <batuhanberke.yildirim@gmail.com>
2023-03-26 20:25:57 +0300

    Initial commit

---
+++ src/main/java/com/comp204/comp204hms/Comp204hmsApplication.java
@@ -0,0 +1,13 @@
+package com.comp204.comp204hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp204hmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp204hmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comp204/comp204hms/Comp204hmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp204.comp204hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp204hmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
