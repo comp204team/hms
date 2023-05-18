1c8fdf1c0e23d85894912a322bab17b1d266c143
BBerkeYildirim <batuhanberke.yildirim@gmail.com>
2023-05-18 22:45:15 +0300

    fix

--- src/main/java/com/comp204/comp204hms/mapper/DepartmentMapper.java
+++ src/main/java/com/comp204/comp204hms/mapper/DepartmentMapper.java
@@ -2,28 +2,15 @@ package com.comp204.comp204hms.mapper;
 
 import com.comp204.comp204hms.dto.department.DepartmentDto;
 import com.comp204.comp204hms.dto.department.DepartmentRequestDto;
-import com.comp204.comp204hms.dto.doctor.DoctorDto;
 import com.comp204.comp204hms.model.Department;
-import com.comp204.comp204hms.model.Doctor;
-import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
-
-import java.util.List;
 
 @Mapper
 public interface DepartmentMapper {
 
     DepartmentMapper INSTANCE = Mappers.getMapper(DepartmentMapper.class);
     DepartmentDto departmentToDepartmentDto(Department department);
-    @Named(value = "doctorToDoctorDto")
-    @Mapping(target = "departmentName", source = "department.name", ignore = true)
-    DoctorDto doctorToDoctorDto(Doctor doctor);
-    @IterableMapping(qualifiedByName = "doctorToDoctorDto")
-    List<DoctorDto> doctorListToDoctorDtoList(List<Doctor> doctors);
-    Department departmentDtoToDepartment(DepartmentDto departmentDto);
 
     Department departmentRequestDtoToDepartment(DepartmentRequestDto departmentRequestDto);
 }
